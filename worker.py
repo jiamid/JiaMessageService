a058f38769197ada97db9825f49d3858ba4bd5f5
jiamid <jiamid@qq.com>
2025-04-10 23:21:46 +0800

    add init browser

--- worker.py
+++ worker.py
@@ -3,4 +3,5 @@ from browser_manager.manager import BrowserManager
 if __name__ == '__main__':
     api_pre = 'http://127.0.0.1:9998'
     browser_manager = BrowserManager(api_pre)
+    browser_manager.init_browsers()
     browser_manager.run()
