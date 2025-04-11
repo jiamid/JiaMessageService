dcc4405a0190df4ef0a485df7756458dc09d7156
Jiamid <JiamidTan@poweronlabs.ai>
2025-04-11 23:07:16 +0800

    change ip

--- worker.py
+++ worker.py
@@ -1,7 +1,7 @@
 from browser_manager.manager import BrowserManager
 
 if __name__ == '__main__':
-    api_pre = 'http://127.0.0.1:9998'
+    api_pre = 'http://43.160.195.19'
     browser_manager = BrowserManager(api_pre)
     browser_manager.init_browsers()
     browser_manager.run()
